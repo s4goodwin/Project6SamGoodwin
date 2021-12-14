06b1860d9f7526021026ff00ae0fe66b63b86a84
s4goodwin <90581608+s4goodwin@users.noreply.github.com>
2021-12-14 18:00:07 -0500

    started testDoesHit method in RectangleTest.java

--- src/main/java/bsu/comp152/RectangleTest.java
+++ src/main/java/bsu/comp152/RectangleTest.java
@@ -1,4 +1,16 @@
 package bsu.comp152;
 
+import org.junit.Test;
+
+import java.awt.*;
+
 public class RectangleTest {
+
+    @Test
+    public void testDoesHit(){
+        int uLeftX=10;
+        int uLeftY=10;
+        int lRightX=10;
+        int lRightY=10;
+    }
 }
